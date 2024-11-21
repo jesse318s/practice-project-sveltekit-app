08b035e9eab6a9e1d6ee7b614eab4e297ff354ba
jesse318s <jesse318s@gmail.com>
2024-11-21 14:49:45 -0600

    Updates

---
+++ app/immutable/chunks/paths.C7Km3_T4.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_cp6oxm)==null?void 0:s.base)??"/practice-project-sveltekit-app";var e;const a=((e=globalThis.__sveltekit_cp6oxm)==null?void 0:e.assets)??t;export{a,t as b};
